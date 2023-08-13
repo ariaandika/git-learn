070fb2c9466358c7259577f89505dc5c4c81ff6e
ariaandika <andikaakun17@gmail.com>
2023-08-13 19:51:12 +0700

    proper fix new js

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ console.log("me")
 
 for (let index = 0; index < array.length; index++) {
   const element = array[index];
-
+  setTimeout(() => {
+    console.trace('me nice')
+  }, addme(89));
   function addme(params) {
     return params + 9 * 0 / 0
   }
@@ -13,4 +15,7 @@ for (let index = 0; index < array.length; index++) {
   }, addme(89));
   addme.call(addme)
   console.info(element)
+  setTimeout(() => {
+    console.trace('me nice')
+  }, addme(89));
 }
