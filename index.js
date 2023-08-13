9fc62f49101e0b76dd1ae0338a49b22fea3261f6
ariaandika <andikaakun17@gmail.com>
2023-08-13 18:49:40 +0700

    edit new js

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+
+  function addme(params) {
+    return params + 9 * 0 / 0
+  }
+  setTimeout(() => {
+    console.trace('me nice')
+  }, addme(89));
+  addme.call(addme)
+  console.info(element)
+}
