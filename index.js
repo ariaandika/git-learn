0f1eaa2efddafe82c0ecde670ddafe6e20013fa2
ariaandika <andikaakun17@gmail.com>
2023-08-13 19:00:33 +0700

    fix indexjs

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ console.log("me")
 
 for (let index = 0; index < array.length; index++) {
   const element = array[index];
+  setTimeout(() => {
+    console.trace('me nice')
+  }, index * 9);
+}
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
 
   function addme(params) {
     return params + 9 * 0 / 0
